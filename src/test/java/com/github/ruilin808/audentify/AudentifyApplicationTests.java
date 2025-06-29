ab271da162524550168206ad6e55335ccae7ac26
ruilin808 <ruilin808@gmail.com>
2025-06-29 00:52:11 -0700

    spring boot file

---
+++ src/test/java/com/github/ruilin808/audentify/AudentifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ruilin808.audentify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudentifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
